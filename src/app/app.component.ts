a20ebfffe09b89ef838b26309b579e38138a8133
Kenny Day <dimxasnewfrozen@gmail.com>
2021-03-29 09:24:50 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cuttingboard';
+
+  public realWidth = 10;
+  public pixelWidth = 800;
+  public columns: string = "2";
+  public rows = new Array(4);
+
+  public columnWidth: number = 0;
+  public halfWidth: number = 0;
+  public totalColumns = new Array(this.columns);
+
+  public result = "";
+
+  ngOnInit(): void {
+    this.calculate();
+  }
+
+  calculate() {
+    if (this.columns == "" || this.rows.length == 0)
+      return;
+
+    this.totalColumns = new Array(parseInt(this.columns));
+    this.columnWidth = this.pixelWidth / (parseInt(this.columns) + .5);
+    this.halfWidth = this.columnWidth / 2;
+
+    const pieceWidthResult = this.realWidth / (parseInt(this.columns) + .5);
+    
+    this.result = this.columns + " pieces at " + pieceWidthResult + " (mm/inches) and 1 at " + pieceWidthResult/2 + " (mm/inches)";
+  }
 }
+
+
